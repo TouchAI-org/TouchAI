1c26d650735ad27e645f142af533d931f8793561
xlgzsgf <hiqiancheng@126.com>
2026-04-06 03:35:20 +0800

    feat(scheduler): add service export interface

---
+++ src/services/SchedulerService/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2026. 千诚. Licensed under GPL v3.
+
+export { eventTriggerRegistry } from './eventRegistry';
+export { TaskScheduler, taskScheduler } from './scheduler';
+export type { CreateScheduledTaskInput, UpdateScheduledTaskInput } from './scheduler';
+export type * from './types';
